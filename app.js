64dcec0765379c380b9e5c50c3abbb38608bc1e1
andreamaluta <andreamaluta@gmail.com>
2025-04-17 15:30:54 +0200

    [UPD] initialized the app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello world!')
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.listen(port, () => {
+    console.log('server aperto sulla porta ' + port);
+})
