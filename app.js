504387b68e919dfe778e7433c4691d12a716a737
andreamaluta <andreamaluta@gmail.com>
2025-04-17 15:48:27 +0200

    [UPD] made the index operation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 
 const port = 3000;
 
+const postsRouter = require('./routers/posts.js');
+app.use('/posts', postsRouter);
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 })
